5c2f267297c73a7d68e4927e420b7014ddc6d68a
emcompany <ericmathewem8@gmail.com>
2023-06-18 12:55:24 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,32 @@ const express = require('express')
 const app = express();
 const PORT = 4578
 const mongoose = require('mongoose');
+const cors = require('cors')
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true})); // to render form data from frontend
+
+
+
 
 mongoose.connect("mongodb+srv://ericmathewem8:Skyline123@cluster0.lfkhqmn.mongodb.net/")
 .then (()=>{console.log("MongoDB connected succussfully")})
 .catch(()=>{console.log("Error occured")})
 
-app.get('/', (req,res)=>{
-    res.send('welcome')
+app.post('/addData', (req, res) => {
+    try {
 
+        let item = req.body
+        console.log(item)
+
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.get('/about',(req,res)=>{
-    res.send('open')
-})
 
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`)
-
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 });
