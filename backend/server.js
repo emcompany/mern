ee47f761e3e1726befa3367282a7b5209d53b04b
emcompany <ericmathewem8@gmail.com>
2023-06-18 16:02:25 +0530

    newupdate

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,22 @@ app.use(express.urlencoded({extended:true})); // to render form data from fronte
 
 
 
-mongoose.connect("mongodb+srv://ericmathewem8:Skyline123@cluster0.lfkhqmn.mongodb.net/")
+mongoose.connect("mongodb+srv://ericmathewem8:Skyline123@cluster0.lfkhqmn.mongodb.net/MES")
 .then (()=>{console.log("MongoDB connected succussfully")})
 .catch(()=>{console.log("Error occured")})
 
-app.post('/addData', (req, res) => {
+const PRODUCT = require("./model/product")
+
+app.post('/addData', async(req, res) => {
     try {
 
         let item = req.body
         console.log(item)
+
+        const saveData = await PRODUCT(item) //PRODUCT is a model. we cross check the form data with the model we created before saving it
+        await saveData.save()  // through this code we save the incoming data from front end to db 
+        res.send((saveData))
+
 
 
 
